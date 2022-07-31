3467c2affcf26978e180a1ee9dff3b7dca0c00a8
Vignesh <vigneshrsb99@gmail.com>
2022-07-31 08:51:09 -0400

    DetailsBid

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
 
-const DetailsBid = () => {
+import {EthPrice} from './SubInfo';
+import {COLORS, SIZES, FONTS} from '../constants';
+
+const DetailsBid = ({bid}) => {
   return (
-    <View>
-      <Text>DetailsBid</Text>
+    <View
+    style={{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: SIZES.base,
+        paddingTop: SIZES.base*2,
+    }}
+    >
+      <Image
+      source={bid.image}
+      resizeMode = "contain"
+      style={{width: 48, height: 48}}
+      />
+      <View>
+        <Text
+        style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small-2,
+            color: COLORS.primary,
+            marginTop: 3,
+        }}
+        >
+            Bid placed by {bid.name}
+        </Text>
+      </View>
+
+      <EthPrice 
+      price={bid.price}
+      />
     </View>
   )
 }
